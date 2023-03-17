027f0358b5b1f17bc181b0076e47f6d1e6d0cc19
ibrahim-sert <evo3506@gmail.com>
2023-03-17 23:56:04 +0300

    movie finished

--- src/components/row/Row.jsx
+++ src/components/row/Row.jsx
@@ -1,8 +1,33 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+import Movie from '../movie/Movie'
 
 const Row = ({title,fetchURL}) => {
+
+    const [movies, setMovies] = useState([])
+  
+
+    useEffect(() => {
+        axios.get(fetchURL).then((res)=>{
+            setMovies(res.data.results)
+        })
+      
+    }, [fetchURL])
+    
+
   return (
-    <div>Row</div>
+    <>
+        <h2 className='text-white font-bold md:text-xl p-4'>{title}</h2>
+        <div className="relative flex items-center">
+            <div id={'slider'}>
+                {movies.map((item,id)=>(
+                   <Movie key={id} item={item}/>
+                ))}
+
+            </div>
+        </div>
+
+    </>
   )
 }
 
